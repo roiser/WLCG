1a1c32af346c03f6059fdb5fc55c36de0929d738
LHCb Dirac Service Account <lbdirac@pcites03.cern.ch>
2013-11-27 15:01:38 +0100

    new probes for num of squids, stratum ones, time stamp

--- ssbCvmfsStatus/wlcg-cvmfs4ssb.py
+++ ssbCvmfsStatus/wlcg-cvmfs4ssb.py
@@ -14,17 +14,13 @@ class c4s :
     self.topologyURL = 'http://lhcb-web-dirac.cern.ch/topology/lhcb_topology.xml'
     self.wlcgBaseUrl = 'http://wlcg-mon.cern.ch/dashboard/request.py/'
     self.wlcgGetUrl = self.wlcgBaseUrl+'getplotdata?columnid=%d&time=24&sites=all&batch=1'
-    self.ssbMetrics = ['CvmfsVersion','CvmfsRepoRevision','CvmfsMountPoint','CvmfsCondDBMountPoint']
-#     self.ssbColumns = {'INFO: CVMFS version installed ' : 'CvmfsVersion',
-#                        'INFO: repository revision ' : 'CvmfsRepoRevision',
-#                        'INFO: Variable VO_LHCB_SW_DIR points to CVMFS mount point ' : 'CvmfsMountPoint',
-#                        'INFO: repository /cvmfs/lhcb-conddb.cern.ch availabl' : 'CvmfsCondDBMountPoint',
-#                        'INFO: Mandatory mount point /cvmfs/lhcb.cern.ch is nfs mount poin' : 'CvmfsViaNfs'
-#                        }
+    self.ssbMetrics = ['CvmfsVersion','CvmfsRepoRevision','CvmfsMountPoint','CvmfsCondDBMountPoint', 'CvmfsProbeTime', 'CvmfsStratumOnes', 'CvmfsNumSquids']
     self.ssbData = {}
     for k in self.ssbMetrics : self.ssbData[k] = {}
 
 
+  ### start probe functions ###
+  ### eval functions ###
 
   def evalCvmfsVersion(self, val): 
     if val == 'nfs' : return (val, 'green')
@@ -49,9 +45,22 @@ class c4s :
     if val == 'yes' : return (val, 'orange')
     else : return (val, 'green')
 
+  def evalCvmfsProbeTime(self, val):
+    return (val, 'green')
 
+  def evalCvmfsStratumOnes(self, val) :
+    if val : return (val, 'green')
+    else: return ('none', 'red')
 
-  def getValCvmfsVersion(self, site, probe):
+  def evalCvmfsNumSquids(self, val):
+    if val :
+      if int(val) > 1 : return (val, 'green')
+      else : return (val, 'orange')
+    else: return (val , 'red')
+
+  ### retrieval functions ###
+
+  def getValCvmfsVersion(self, site, probe, metric):
     pat1 = 'INFO: CVMFS version installed '
     pat2 = 'INFO: Mandatory mount point /cvmfs/lhcb.cern.ch is nfs mount point'
     ver = 'n/a'
@@ -62,7 +71,7 @@ class c4s :
         ver = 'nfs'
     self.ssbData['CvmfsVersion'][site] = ver
     
-  def getValCvmfsRepoRevision(self, site, probe):
+  def getValCvmfsRepoRevision(self, site, probe, metric):
     pat = 'INFO: repository revision '
     rev = 'n/a'
     for line in probe :
@@ -71,7 +80,7 @@ class c4s :
         break 
     self.ssbData['CvmfsRepoRevision'][site] = rev
 
-  def getValCvmfsMountPoint(self, site, probe):
+  def getValCvmfsMountPoint(self, site, probe, metric):
     pat = 'INFO: Variable VO_LHCB_SW_DIR points to CVMFS mount point '
     mp = 'n/a'
     for line in probe :
@@ -79,7 +88,7 @@ class c4s :
         mp = line[len(pat):]
     self.ssbData['CvmfsMountPoint'][site] = mp
 
-  def getValCvmfsCondDBMountPoint(self, site, probe):
+  def getValCvmfsCondDBMountPoint(self, site, probe, metric):
     pat = 'INFO: repository /cvmfs/lhcb-conddb.cern.ch available'
     cm = 'no'
     for line in probe :
@@ -87,31 +96,42 @@ class c4s :
         cm = 'yes'
     self.ssbData['CvmfsCondDBMountPoint'][site] = cm
 
+  def getValCvmfsProbeTime(self, site, probe, metric):
+    self.ssbData['CvmfsProbeTime'][site] = metric['Time']
+
+  def getValCvmfsStratumOnes(self, site, probe, metric) :
+    strats = []
+    pat = 'INFO: Servers: '
+    for line in probe :
+      if line[:len(pat)] == pat :
+        stratumL = line[len(pat):]
+        for serv in stratumL.split() :
+          strats.append('.'.join(serv.split('/')[2].split(':')[0].split('.')[-2:]))
+        break
+    self.ssbData['CvmfsStratumOnes'][site] = ' '.join(strats)
+
+  def getValCvmfsNumSquids(self, site, probe, metric) :
+    numSq = 0
+    pat = 'INFO: Proxies: '
+    for line in probe :
+      if line[:len(pat)] == pat :
+        numSq = len(line[len(pat):].split())
+        break
+    self.ssbData['CvmfsNumSquids'][site] = numSq
+
+  ### end probe functions ####
+
 
   def populateTopology(self):
     topo = json.loads(urllib.urlopen(self.wlcgGetUrl%self.wlcgTopoColumnNo).read())
     for ent in topo['csvdata'] : self.topoDict[self.myVO][ent['SiteId']] = ent['Status']
 
   def collectInfo(self):
-#    matchLines = self.ssbColumns.keys()
     info = json.loads(urllib.urlopen(self.wlcgGetUrl%self.cvmfsColumnNo).read())
-    for metric in info['csvdata'] :
-      itemFound = []
-      site = self.topoDict[self.myVO][metric['SiteId']]
-      tinfo = urllib.urlopen(self.wlcgBaseUrl+metric['URL']).read()
-      tl = tinfo.split('\n')
-      for metric in self.ssbMetrics : eval('self.getVal'+metric)(site, tl)
-####
-#       for tl in tinfo.split('\n') :
-#         for ml in matchLines :
-#           if tl[:len(ml)] == ml :
-#             if self.ssbColumns[ml] == 'CvmfsViaNfs' : ml = 'INFO: CVMFS version installed '
-#             #if not self.ssbData[self.ssbColumns[ml]].has_key(site) :
-#             if self.ssbColumns[ml] not in itemFound : 
-#               itemFound.append(self.ssbColumns[ml])
-#               self.ssbData[self.ssbColumns[ml]][site] = tl[len(ml):]
-####
-
+    for metricInf in info['csvdata'] :
+      site = self.topoDict[self.myVO][metricInf['SiteId']]
+      tl = urllib.urlopen(self.wlcgBaseUrl+metricInf['URL']).read().split('\n')
+      for metr in self.ssbMetrics : eval('self.getVal'+metr)(site, tl, metricInf)
               
   def writeSSBColumns(self):
     for k in self.ssbMetrics :
